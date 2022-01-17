16297561bad8f582564ea40fec0b119ce3974060
Arsh <arshgoyal2005@gmail.com>
2022-01-17 19:07:57 +0530

    added menu btn click event

--- src/Components/NavBar.js
+++ src/Components/NavBar.js
@@ -2,10 +2,15 @@ import React from 'react';
 import './Styles/NavBar.scss';
 
 const NavBar = () => {
+
+  let clickHandler = () => {
+    console.log("btn clicked");
+  }
+
   return (
     <nav>
       <h2 className='app-name'>Weather</h2>
-      <div className='menu-btn'>
+      <div className='menu-btn' onClick={clickHandler}>
         <span></span>
         <span></span>
       </div>
