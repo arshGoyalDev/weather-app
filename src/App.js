23763ae4a2474344cc3818919fbdbceeffee9c8b
Arsh <arshgoyal2005@gmail.com>
2022-01-30 20:27:13 +0530

    fixed a big issue

---
+++ src/App.js
@@ -0,0 +1,117 @@
+import React, { useState, useEffect } from "react";
+import "./components/styles/App.scss";
+
+import { getCurrentData, getForecastData, getCondition, getPreferredTheme } from "./utils";
+
+import NavBar from "./components/NavBar";
+import Details from "./components/Details";
+import Progress from "./components/Progress";
+import Weather from "./components/Weather";
+import OtherDetailsMenu from "./components/OtherDetailsMenu";
+
+const App = () => {
+  const [currentData, setCurrentData] = useState({});
+  const [forecastData, setForecastData] = useState([]);
+  const [hourlyData, setHourlyData] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [hide, setHide] = useState(false);
+  const [visible, setVisible] = useState(false);
+  const [condition, setCondition] = useState("");
+  const [unit, setUnit] = useState("metric");
+  const [otherDetailsMenu, setOtherDetailsMenu] = useState(false);
+  // const [theme, setTheme] =
+
+  if (currentData.location !== undefined) {
+    setTimeout(() => {
+      setHide(true);
+
+      setTimeout(() => {
+        setVisible(true);
+      }, 1000);
+    }, 1000);
+  }
+
+  console.log(getPreferredTheme());
+
+  // get current location from navigator geolocation
+  const getLocation = () => {
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(fetchCurrentData);
+    } else {
+      alert("Geolocation is not supported by this browser.");
+    }
+  };
+
+  // fetch current and  hourly forecast data for current location
+  let fetchCurrentData = (position, location) => {
+    getCurrentData(position, location).then((data) => {
+      setCurrentData(data);
+      let current = new Date(data.current.last_updated).getHours();
+      setHourlyData(
+        data.forecast.forecastday[0].hour.slice(current - 2, current + 3)
+      );
+
+      setLoading(false);
+    });
+  };
+
+  useEffect(() => {
+    if (currentData.location !== undefined) {
+      getForecastData(
+        currentData.location.lat,
+        currentData.location.lon,
+        unit
+      ).then((data) => {
+        setForecastData(data.daily.slice(1, 8));
+      });
+
+      setCondition(getCondition(currentData.current.condition.text));
+    }
+  }, [currentData, unit]);
+
+  return (
+    <main className="App">
+      <div className={`background-wrapper ${visible ? condition : ""}`}></div>
+      <section className={`main ${hide ? "active" : ""}`}>
+        <NavBar
+          hide={hide}
+          otherDetailsMenu={otherDetailsMenu}
+          setOtherDetailsMenu={setOtherDetailsMenu}
+        />
+        {hide ? (
+          <Weather
+            visible={visible}
+            currentData={currentData.current}
+            unit={unit}
+          />
+        ) : (
+          <Progress currentData={currentData} hide={hide} />
+        )}
+        <Details
+          getLocation={getLocation}
+          fetchCurrentData={fetchCurrentData}
+          currentData={currentData}
+          loading={loading}
+          setLoading={setLoading}
+          hide={hide}
+          visible={visible}
+        />
+      </section>
+      {hide ? (
+        <OtherDetailsMenu
+          currentData={currentData}
+          forecastData={forecastData}
+          hourlyData={hourlyData}
+          otherDetailsMenu={otherDetailsMenu}
+          setOtherDetailsMenu={setOtherDetailsMenu}
+          unit={unit}
+          setUnit={setUnit}
+        />
+      ) : (
+        ""
+      )}
+    </main>
+  );
+};
+
+export default App;
---
+++ src/Components/Details.js
@@ -0,0 +1,122 @@
+import React, { useState } from "react";
+import "./styles/Details.scss";
+
+const Details = ({
+  getLocation,
+  fetchCurrentData,
+  currentData,
+  loading,
+  setLoading,
+  hide,
+  visible,
+}) => {
+  const [locationChangeVisibility, setLocationChangeVisibility] =
+    useState(false);
+  const [location, setLocation] = useState("");
+
+  // get current date and time
+  let parts = (
+    currentData.location !== undefined
+      ? currentData.location.localtime.substring(0, 10)
+      : ""
+  ).split("-");
+  let currentDate = new Date();
+  let date = new Date(parts[0], parts[1] - 1, parts[2]);
+
+  // handler for clicks on track btn
+  let changeLocation = (e) => {
+    loading ? setLoading(false) : setLoading(true);
+    locationChangeVisibility
+      ? setLocationChangeVisibility(false)
+      : setLocationChangeVisibility(true);
+  };
+
+  // track current location
+  let trackLocation = () => {
+    getLocation();
+    setLocationChangeVisibility(false);
+  };
+
+  // update the location when the value changes
+  let changeHandler = (e) => {
+    setLocation(e.target.value);
+  };
+
+  // press enter to fetch current data
+  let keyDownHandler = (e) => {
+    if (e.keyCode === 13) {
+      fetchCurrentData("", location);
+      setLocationChangeVisibility(false);
+    }
+  };
+
+  return (
+    <div className="details">
+      <div className={`details--time ${hide ? "animate-left" : ""}`}>
+        {visible ? (
+          <>
+            <span>{currentData.location.localtime.substring(11, 16)}</span>{" "}
+            <span className="dash"></span>{" "}
+            <span>
+              {date.toLocaleDateString("en-US", { weekday: "long" })},{" "}
+              {date.getDate()}{" "}
+              {date.toLocaleString("default", { month: "short" })}{" "}
+              {date.getFullYear()}
+            </span>
+          </>
+        ) : (
+          <>
+            <span>{currentDate.toTimeString().substring(0, 5)}</span>{" "}
+            <span className="dash"></span>{" "}
+            <span>
+              {currentDate.toLocaleDateString("en-US", { weekday: "long" })},{" "}
+              {currentDate.getDate()}{" "}
+              {currentDate.toLocaleString("default", { month: "short" })}{" "}
+              {currentDate.getFullYear()}
+            </span>
+          </>
+        )}
+      </div>
+
+      <div className={`details--location ${hide ? "animate-right" : ""}`}>
+        <span className="location">
+          {visible
+            ? `${currentData.location.name}, ${currentData.location.country}`
+            : "Location"}
+        </span>
+        <button className="change-btn" onClick={changeLocation}>
+          {loading ? (
+            <div className="loader">
+              <span className="dot dot-one"></span>
+              <span className="dot dot-two"></span>
+              <span className="dot dot-third"></span>
+            </div>
+          ) : (
+            "change"
+          )}
+        </button>
+        <div
+          className={`details--location-change ${
+            locationChangeVisibility ? "show" : ""
+          }`}
+        >
+          <button className="track-btn" onClick={trackLocation}>
+            track
+          </button>
+          <span>Or</span>
+          <input
+            type="text"
+            name="location"
+            className="location-input"
+            value={location}
+            placeholder="Enter location"
+            onChange={changeHandler}
+            onKeyDown={keyDownHandler}
+          />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Details;
