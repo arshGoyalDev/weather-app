56034491bbcf42c2989060ab97d6704e8555c783
Arsh <arshgoyal2005@gmail.com>
2022-02-04 19:59:03 +0530

    installed font-awesome to use add icon

--- src/Components/OtherLocations.js
+++ src/Components/OtherLocations.js
@@ -1,16 +1,22 @@
 import React, { useState } from "react";
 import "./Styles/OtherLocations.scss";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faPlus } from '@fortawesome/free-solid-svg-icons'
 
 const OtherLocations = () => {
   const [newLocation, setNewLocation] = useState("");
   const [adding, setAdding] = useState(false);
+
+  const showInput = () => {
+    adding ? setAdding(false) : setAdding(true);
+  }
 
   return (
     <div className="other-locations">
       <h4>Additional Locations</h4>
       <div className="other-locations--add-new-location">
         {adding ? (
-          <div className="input">
+          <div className="add-location-input">
             <input
               type="text"
               placeholder="Enter location"
@@ -19,6 +25,7 @@ const OtherLocations = () => {
                 setNewLocation(e.target.value);
               }}
             />
+            <button className="add-new-location-btn">Add</button>
           </div>
         ) : (
           <div className="information">
@@ -31,9 +38,8 @@ const OtherLocations = () => {
             </div>
           </div>
         )}
-        <div className="add-illustration">
-          <span></span>
-          <span></span>
+        <div className="add-illustration" onClick={showInput}>
+          <FontAwesomeIcon icon={faPlus} />
         </div>
       </div>
 
