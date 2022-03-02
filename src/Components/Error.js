f2a73c3a5b63cf14c6cf788518d1ccb6c9104902
arshWebDev <arshgoyalDev@gmail.com>
2022-03-02 14:42:03 +0530

    fixed some issues in error component

--- src/Components/Error.js
+++ src/Components/Error.js
@@ -2,22 +2,32 @@ import "./Styles/Error.scss";
 import ErrorSvg from "../assets/Images/undraw_location_search_re_ttoj.svg";
 
 const Error = ({ error, setError, errorStatement }) => {
+  console.log(error);
+
   return (
-    <div
-      className={`error-wrapper ${!error && "hidden"}`}
-      onClick={() => {
-        setError(false);
-      }}
-    >
-      <div className="error">
+    <>
+      <div
+        className={`error-wrapper ${!error ? "hidden" : ""}`}
+        onClick={() => {
+          setError(false);
+        }}
+      ></div>
+      <div className={`error ${!error ? "hidden" : ""}`}>
         <div className="error-svg">
           <img src={ErrorSvg} alt="error illustration from undraw.co" />
         </div>
         <h1>Error</h1>
         <p>{errorStatement}</p>
-        <button className="retry-btn">Retry</button>
+        <button
+          className="retry-btn"
+          onClick={() => {
+            setError(false);
+          }}
+        >
+          Retry
+        </button>
       </div>
-    </div>
+    </>
   );
 };
 
